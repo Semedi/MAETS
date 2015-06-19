cc047a2de4e60404a951d202f13decd14f9551a5
Alba <albissweet90@gmail.com>
2015-06-19 12:26:02 +0200

    nueva pagPpalUser

---
+++ Usuario/pagPpalUser2.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<title> MAETS </title>
+	<link rel="icon" type="image/png" href="../images/MAETS.png" />
+
+  <link rel="stylesheet" type="text/css" href="../css/main.css" />
+  <link rel="stylesheet" type="text/css" href="../css/users.css" />
+	
+</head>
+
+
+<?php  include ('../static/mainTOP.php'); ?> 
+
+
+	    	
+        <div id ="friendsContent">
+            <div id = "user_friends_header">
+              <img id="imagen_Avatar" src='/MAETS/images/userProfile.png'>
+               <div id = "texto_perfil" >
+                  <h2><?php echo $_SESSION["Nick"]; ?></h2>
+                  <img id = "bandera_nacionalidad" src="../images/usuarios/SpainFlag.jpg">
+               </div>
+                <div id = "botonAniadir">
+                   <button type="button" id="addFriendButton">Añadir a mis amigos</button>
+                  </div>
+            </div>
+            <div id ="mainContent">
+              <h1> Perfil del usuario</h1>
+
+              <table>
+                <tr><h2> Resumen </h2></tr>
+                
+
+                <tr>
+                    <td>Nick:</td>
+                    <td><?php echo $_SESSION["Nick"]; ?></td>
+                </tr>
+         
+                <tr>
+                    <td>Nombre: </a></td>
+                    <td><?php echo $_SESSION['Nombre']; ?></td>
+                </tr>
+         
+                 <tr>
+                    <td>Apellidos: </a></td>
+                    <td><?php echo $_SESSION['Apellidos']; ?></td>
+                </tr>
+
+                        <tr>
+                    <td>Correo: </a></td>
+                    <td><?php echo $_SESSION["Email"]; ?></td>
+                </tr>
+
+                         <tr>
+                    <td>Juegos comprados: </a></td>
+                    <td>5</td>
+                </tr>
+
+                       <tr>
+                    <td>Puntos de logro: </a></td>
+                    <td>52</td>
+                </tr>
+              </table>
+            </div>
+           
+        </div>
+	  
+	  
+<?php  include ('../static/mainBOT.php'); ?>  
+
+	
+</html>
