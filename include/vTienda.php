06d6cc99283adb925fe2bc7d5ee0580e0e6a9cee
admachin <adolfomachin@yahoo.es>
2015-06-20 18:38:04 +0200

    Lista tienda terminada

--- include/vTienda.php
+++ include/vTienda.php
@@ -12,7 +12,7 @@ function generarLista($tipo){
 
 			for($i=0; $i<sizeof($res)-1; $i++) {
 
-				echo "<div id='juego' onclick='location.href='articulo-tienda.php'' style='cursor:pointer'>";
+				echo "<div id='juego' onclick=location.href='articulo-tienda.php' style='cursor:pointer'>";
 	    				echo "<img class='imagen' src='images/Portadas/" .$res[$i]['Portada']. "'>";
 	    				echo "<div id='titulo'>";
 	    					echo "<p class='titulo'><b>" .$res[$i]['Titulo']. " </b> </p>";
