c2726aa997a688905592e1b0963dbbe27996ed9d
Drulas <javier_druet_93@hotmail.com>
2015-06-20 20:38:01 +0200

    creada columna fecha en tabla analisis para traer los mas nuevos, creados los metodos de traer capturas y analisis de la bd y actualizadas las consultas de usuario sobre sus puntos, juegos y demas que ya devuelven el resultado

--- include/communityBD.php
+++ include/communityBD.php
@@ -19,4 +19,39 @@ function getForoBD()
 		closeConnection($con);
 		return ($retorno);
 	}
+
+
+
+function getLastCaptures(){
+	$connection = createConnection();
+
+	$sql = "SELECT Ruta FROM capturas ORDER BY fecha DESC LIMIT 4"; 
+
+    $res = $connection->query($sql) or die ($connection->error. " en la linea". (_LINE_-1));
+
+	while($ret[] = $res->fetch_assoc());
+
+	closeConnection($connection);
+
+	return($ret);
+	
+}
+
+function getLastAnalisis(){
+	$connection = createConnection();
+
+	$sql = "SELECT * FROM analisis ORDER BY fecha DESC LIMIT 4"; 
+
+    $res = $connection->query($sql) or die ($connection->error. " en la linea". (_LINE_-1));
+
+	while($ret[] = $res->fetch_assoc());
+
+	closeConnection($connection);
+
+	return($ret);
+}
+
+
+
+
 ?>
