4881e073a7e1b5a7b1066c4a14c7fd42f07c40d9
admachin <adolfomachin@yahoo.es>
2015-06-21 22:16:55 +0200

    cambios

--- include/shopBD.php
+++ include/shopBD.php
@@ -78,7 +78,7 @@ function insertGame($titulo, $precio, $edad, $etiquetas, $descripcion, $descripc
 	$sql = "SELECT * FROM juego WHERE Titulo = '$titulo'";
 	$result = $connection->query($result) or die ($connecion->error. " en la linea".(_LINE_-1));
 
-	if($row = result->fetch_assoc())
+	if($row = $result->fetch_assoc())
 		echo "El juego ya existe.";
 	else {
 		$hoy = gmdate('Y-m-d');
@@ -86,33 +86,6 @@ function insertGame($titulo, $precio, $edad, $etiquetas, $descripcion, $descripc
 				VALUES (NULL, '$titulo', '$portada', '$precio', '$edad', '$descipcion', '$descripcionLarga', '$tipoJuego', '$idiomas', '$etiquetas', '0', '$hoy'";
 		$connection->query($sql) or die ($connecion->error. " en la linea".(_LINE_-1));
 	}	
-	closeConnection($connection);
-}
-
-$connection = createConnection();
-	
-	$result = "SELECT * FROM usuario WHERE nick = '$user'";
-	$result = $connection->query($result) or die ($connection->error. " en la linea".(_LINE_-1));
-
-
-	if($row = $result->fetch_assoc())
-		{     
-			echo "Ya existe este usuario";
-		}
-	else
-		{
-			$nombreSal = $user;
-			$passSal = $pass;
-			
-			$salt = substr(base64_encode(openssl_random_pseudo_bytes('30')), 0, 22);
-			$salt = strtr($salt, array('+' => '.')); 
-			$hash = crypt($passSal, '$2y$10$' . $salt);
-
-			$q = "INSERT INTO `usuario` (`Id`, `Nick`, `Contrasenia`, `Nombre`, `Apellidos`, `Correo`, `Fecha de Nacimiento`, `Pais`, `Ciudad`, `Direccion`, `Codigo Postal`, `Puntuacion`, `Rol`, `Imagen` , `Companyia`)
-				VALUES (NULL, '$user' , '$hash', '', '', '$mail', '0000-00-00', '', '', '', 00000, 0 , 'Usuario Registrado', 'usuario.jpg', '')";
-			$connection->query($q) or die($connection->error. " en la linea".(_LINE_-1));
-		}
-
 	closeConnection($connection);
 }
 
