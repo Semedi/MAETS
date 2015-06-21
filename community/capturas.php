89bd2df992835d21ba858cc77875b6ab9d6248e1
adrianmjim <adrianmjim199434@gmail.com>
2015-06-21 20:39:20 +0200

    subirimagen

--- community/capturas.php
+++ community/capturas.php
@@ -19,7 +19,7 @@
 			<div id="capturas">
 				<?php generarCapturas();?>
 				<form enctype="multipart/form-data" action="../uploader.php" method="POST">
-				<input name="uploadedfile" type="file" />
+				<input id="archivo" name="uploadedfile" type="file" />
 				<input type="submit" value="Subir archivo" />
 				</form>
 				
