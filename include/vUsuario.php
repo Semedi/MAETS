6140a19dc1050eb3af36ada80b1d3cd9c1728a1f
Sergio Alfonso Semedi Barranco <sergio.semedi@gmail.com>
2015-06-21 18:21:34 +0200

    busqueda arreglada y avanzando

--- include/vUsuario.php
+++ include/vUsuario.php
@@ -1,4 +1,18 @@
 <?php
 
 
+
+
+function generarListaBusqueda($busqueda, $column, $like){
+	
+	
+		$res = getListaUsuarios($busqueda,$column, $like);
+
+	    	echo "<div id='usuario' onclick=location.href='articulo-tienda.php?user=' style='cursor:pointer'>";
+			echo "<img class='avatar' src='../images/userProfile.png'>";
+			echo "nombre";
+			echo "</div>";
+
+}
+
 ?>
