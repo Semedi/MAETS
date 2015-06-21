74c525a85f87fd7c3976925579870eb681b7f305
adrianmjim <adrianmjim199434@gmail.com>
2015-06-21 20:24:01 +0200

    subircaptura

--- community/capturas.php
+++ community/capturas.php
@@ -18,7 +18,10 @@
 			
 			<div id="capturas">
 				<?php generarCapturas();?>
-				
+				<form enctype="multipart/form-data" action="../uploader.php" method="POST">
+				<input name="uploadedfile" type="file" />
+				<input type="submit" value="Subir archivo" />
+				</form>
 				
 			</div>	
 	    	
