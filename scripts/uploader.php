77fa1655a2f419cf0a787870a91d9beb4567ecde
Sergio Alfonso Semedi Barranco <sergio.semedi@gmail.com>
2015-06-22 22:36:35 +0200

    arreglado uploader2

--- scripts/uploader.php
+++ scripts/uploader.php
@@ -13,9 +13,11 @@
 	}
 
 	$file_name=$_FILES['uploadedfile']['name'];
-	$add="images/screens/$file_name";
+	$add="../images/screens/$file_name";
 	if($uploadedfileload=="true"){
-		include('include/communityBD.php');
+
+		include('../include/communityBD.php');
+
 		insertCaptura($_GET['user'], $_FILES['uploadedfile']['name'], true);
 		if(move_uploaded_file ($_FILES['uploadedfile']['tmp_name'], $add)){
 			echo "<script type='text/javascript'>alert('Ha sido subido satisfactoriamente');</script>";
@@ -25,5 +27,5 @@
 	}else{
 		echo "<script type='text/javascript'>alert('$msg');</script>";
 	}
-	print("<script>window.location.replace('community/capturas.php');</script>"); 
+	print("<script>window.location.replace('../community/capturas.php');</script>"); 
 ?>
