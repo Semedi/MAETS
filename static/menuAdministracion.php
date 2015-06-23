4ba47cddd53238c3aa3a1c83e39a0202238ddfe4
Sergio Alfonso Semedi Barranco <sergio.semedi@gmail.com>
2015-06-23 17:21:16 +0200

    arreglado menu Admin

--- static/menuAdministracion.php
+++ static/menuAdministracion.php
@@ -5,5 +5,5 @@
 	if($_SESSION["Rol"]=='Administrador' or $_SESSION["Rol"]=='Gestor-Comunidad')
 		echo "<p><a href='../admin/gestorComunidad.php'>Administrar comunidad</a></p>";
 	if($_SESSION["Rol"]=='Gestor-Tienda')
-		echo "<p><a href='../gestorJuegos.php'>Administrar juegos</a></p>";
+		echo "<p><a href='../admin/gestorJuegos.php'>Administrar juegos</a></p>";
 ?>
